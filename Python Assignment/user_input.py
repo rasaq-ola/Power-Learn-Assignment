f249c5ef22d841227a8ee36c588ebe3f05c63902
rasaq-ola <115763421+rasaq-ola@users.noreply.github.com>
2024-02-24 11:26:23 -0800

    Add files via upload

---
+++ Python Assignment/user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?: \n")
+age = input("What is your age?: \n")
+location = input("What is your location?: \n")
+print(f"Hello {name}, you are {age} years old and live in {location}")
